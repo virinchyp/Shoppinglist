970f0e0dc3833accb2b82705e2a6f1fe1845addb
Ronny Friedland <mail@ronnyfriedland.de>
2013-04-21 20:49:21 +0200

    some refactorings, disable landscape orientation

--- shoppinglist-app/src/de/ronnyfriedland/shoppinglist/MainActivity.java
+++ shoppinglist-app/src/de/ronnyfriedland/shoppinglist/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_layout);
 
         saveButton = (Button) findViewById(R.id.buttonSave);
         textQuantityValue = (EditText) findViewById(R.id.textViewQuantityValue);
@@ -201,7 +201,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
